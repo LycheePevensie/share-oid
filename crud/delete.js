c3d2e27359b5cd081d17f81fd62f016b77d9a7f4
vincent peng <viper404@163.com>
2016-09-20 15:46:21 +0800

    Add files via upload

---
+++ crud/delete.js
@@ -0,0 +1,24 @@
+var fs = require('fs');
+var path = require('path');
+var output = new Object();
+var target = "1.1.4";
+fs.readFile(path.join(__dirname, 'data.json'), function (err, input) {
+  if (err) {
+    throw err;
+  }
+  var obj = JSON.parse(input);
+  for (var oid in obj) {
+    output[oid] = [].concat(obj[oid]);
+  }
+  var str = "Not Found!";
+  if (output.hasOwnProperty(target)) {
+    str = output[target].toString();
+    delete output[target];
+  }
+  fs.writeFile(path.join(__dirname, 'data.json'), JSON.stringify(output), function (err) {
+    if (err) {
+      throw err;
+    }
+    console.log(str);
+  });
+});
