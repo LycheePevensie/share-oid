4d4a339cd33570bdbf0a3685f8ed3ce67346c5ed
vincent peng <viper404@163.com>
2016-10-01 19:10:45 +0800

    Realize function of RetrieveById in main-process

--- qrcode/main-process/retrieve-by-id.js
+++ qrcode/main-process/retrieve-by-id.js
@@ -1,5 +1,28 @@
 const ipc = require('electron').ipcMain
+const fs = require('fs')
+const path = require('path')
 
-ipc.on('asynchronous-message', function (event, arg) {
-  event.sender.send('asynchronous-reply', 'pong')
+ipc.on('RetrieveById-message', function (event, arg) {
+  let output = new Object()
+  let target = `${arg}`
+  let str = ''
+  fs.readFile(path.join(__dirname, 'data.json'), function (err, input) {
+    if (err) {
+      throw err
+    }
+    let obj = JSON.parse(input)
+    for (let oid in obj) {
+      output[oid] = [].concat(obj[oid])
+    }
+    str = 'Not Found!'
+    if (output.hasOwnProperty(target)) {
+      str = output[target].toString()
+    }
+    fs.writeFile(path.join(__dirname, 'data.json'), JSON.stringify(output), function (err) {
+      if (err) {
+        throw err
+      }
+    });
+  });
+  event.sender.send('RetrieveById-reply', str)
 })
