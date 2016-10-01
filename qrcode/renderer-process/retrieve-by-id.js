920228ea6bbbf0899907011c134ee3363512a38f
vincent peng <viper404@163.com>
2016-10-01 19:11:46 +0800

    Realize function of RetrieveById in renderer-process

--- qrcode/renderer-process/retrieve-by-id.js
+++ qrcode/renderer-process/retrieve-by-id.js
@@ -1,23 +1,14 @@
-const clipboard = require('electron').clipboard
-
-const copyBtn = document.getElementById('copy-to')
-const copyInput = document.getElementById('copy-to-input')
-
-copyBtn.addEventListener('click', function () {
-  if (copyInput.value !== '') copyInput.value = ''
-  copyInput.placeholder = 'Copied! Paste here to see.'
-  clipboard.writeText('Electron Demo!')
-})
-
 const ipc = require('electron').ipcRenderer
 
-const asyncMsgBtn = document.getElementById('async-msg')
+const RetrieveByIdBtn = document.getElementById('retrieve-by-id')
+const RetrieveByIdInput = document.getElementById('retrieve-by-id-input')
 
-asyncMsgBtn.addEventListener('click', function () {
-  ipc.send('asynchronous-message', 'ping')
+RetrieveByIdBtn.addEventListener('click', function () {
+  ipc.send('RetrieveById-message', RetrieveByIdInput.value)
+  RetrieveByIdInput.placeholder = 'Retrieved!'
 })
 
-ipc.on('asynchronous-reply', function (event, arg) {
-  const message = `Asynchronous message reply: ${arg}`
-  document.getElementById('async-reply').innerHTML = message
+ipc.on('RetrieveById-reply', function (event, arg) {
+  let message = `${arg}`
+  document.getElementById('retrieve-by-id-reply').innerHTML = message
 })
