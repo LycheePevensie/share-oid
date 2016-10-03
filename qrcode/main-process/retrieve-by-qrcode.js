9f3c9c89106dd970954dc15da198eadfdc5f6013
vincent peng <viper404@163.com>
2016-10-03 20:13:52 +0800

    Realize function of RetrieveByQrcode in main-process

---
+++ qrcode/main-process/retrieve-by-qrcode.js
@@ -0,0 +1,27 @@
+const ipc = require('electron').ipcMain
+const fs = require('fs')
+const path = require('path')
+
+ipc.on('RetrieveByQrcode-message', function (event, arg) {
+  let output = new Object()
+  let target = `${arg}`
+  fs.readFile(path.join(__dirname, '../data.json'), function (err, input) {
+    if (err) {
+      throw err
+    }
+    let obj = JSON.parse(input)
+    for (let oid in obj) {
+      output[oid] = [].concat(obj[oid])
+    }
+    let str = target+','+'Not Found!'
+    if (output.hasOwnProperty(target)) {
+      str = target+','+output[target].toString()
+    }
+    fs.writeFile(path.join(__dirname, '../data.json'), JSON.stringify(output), function (err) {
+      if (err) {
+        throw err
+      }
+      event.sender.send('RetrieveByQrcode-reply', str)
+    })
+  })
+})
