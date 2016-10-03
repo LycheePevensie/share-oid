b0a16e0519eeb3a27f08b49b533d64a6f3d0ac97
vincent peng <viper404@163.com>
2016-10-03 21:54:07 +0800

    Realize function of UpdateByQrcode in main-process

---
+++ qrcode/main-process/update-by-qrcode.js
@@ -0,0 +1,29 @@
+const ipc = require('electron').ipcMain
+const fs = require('fs')
+const path = require('path')
+
+ipc.on('UpdateByQrcode-message', function (event, arg) {
+  let output = new Object()
+  let arr = `${arg}`.split(',')
+  let target = arr.shift()
+  fs.readFile(path.join(__dirname, '../data.json'), function (err, input) {
+    if (err) {
+      throw err
+    }
+    let obj = JSON.parse(input)
+    for (let oid in obj) {
+      output[oid] = [].concat(obj[oid])
+    }
+    let str = target+','+'Not Found!'
+    if (output.hasOwnProperty(target)) {
+      output[target] = [].concat(arr)
+      str = target+','+output[target].toString()
+    }
+    fs.writeFile(path.join(__dirname, '../data.json'), JSON.stringify(output), function (err) {
+      if (err) {
+        throw err
+      }
+      event.sender.send('UpdateByQrcode-reply', str)
+    })
+  })
+})
