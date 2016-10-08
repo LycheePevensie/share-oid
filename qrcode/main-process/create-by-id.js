b23c58c89c7f0fd2c3e9b7954ddf89277ff02bc3
vincent peng <viper404@163.com>
2016-10-08 17:10:12 +0800

    Update function of CreateById

--- qrcode/main-process/create-by-id.js
+++ qrcode/main-process/create-by-id.js
@@ -4,7 +4,7 @@ const path = require('path')
 
 ipc.on('CreateById-message', function (event, arg) {
   let output = new Object()
-  let arr = `${arg}`.split(',')
+  let arr = `${arg}`.split(';')
   let target = arr.shift()
   fs.readFile(path.join(__dirname, '../data.json'), function (err, input) {
     if (err) {
@@ -14,7 +14,7 @@ ipc.on('CreateById-message', function (event, arg) {
     for (let oid in obj) {
       output[oid] = [].concat(obj[oid])
     }
-    let str = target+','+'Already Have!'
+    let str = 'Already Have '+target+' !'
     if (!output.hasOwnProperty(target)) {
       output[target] = [].concat(arr)
       str = target+','+output[target].toString()
--- qrcode/renderer-process/create-by-id.js
+++ qrcode/renderer-process/create-by-id.js
@@ -2,14 +2,17 @@ const ipc = require('electron').ipcRenderer
 const qr = require('qr-image')
 
 const CreateByIdBtn = document.getElementById('create-by-id')
-const CreateByIdInput = document.getElementById('create-by-id-input')
+const CreateByIdInputIdentifier = document.getElementById('create-by-id-input-identifier')
+const CreateByIdInputName = document.getElementById('create-by-id-input-name')
+const CreateByIdInputPathema = document.getElementById('create-by-id-input-pathema')
+const CreateByIdInputOthers = document.getElementById('create-by-id-input-others')
 
 CreateByIdBtn.addEventListener('click', function () {
-  ipc.send('CreateById-message', CreateByIdInput.value)
-  if (CreateByIdInput.value !== '') {
-    CreateByIdInput.value = ''
-  }
-  CreateByIdInput.placeholder = 'Created!'
+  ipc.send('CreateById-message', '1.2.156.112606.1.2.1.1.'+CreateByIdInputIdentifier.value+';'+CreateByIdInputName.value+';'+CreateByIdInputPathema.value+';'+CreateByIdInputOthers.value)
+  CreateByIdInputIdentifier.value = ''
+  CreateByIdInputName.value = ''
+  CreateByIdInputPathema.value = ''
+  CreateByIdInputOthers.value = ''
 })
 
 ipc.on('CreateById-reply', function (event, arg) {
