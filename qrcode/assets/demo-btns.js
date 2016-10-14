9ff3a1998fd78e5d3c684608dc9d28bfb84007b1
vincent peng <viper404@163.com>
2016-10-14 10:28:15 +0800

    Initialize EMPI data in json storage

--- qrcode/assets/demo-btns.js
+++ qrcode/assets/demo-btns.js
@@ -18,3 +18,13 @@ storage.get('activeDemoButtonId', function (err, id) {
   if (err) return console.error(err)
   if (id && id.length) document.getElementById(id).click()
 })
+
+let InitialEMPIData = {
+  "1.2.156.112606.1.2.1.1.1.110.1980.1111":["张三","糖尿病","无"],
+  "1.2.156.112606.1.2.1.1.2.310.1990.2222":["李四","急性阑尾炎","青霉素"],
+  "1.2.156.112606.1.2.1.1.1.440.2000.3333":["王五","急性肠胃炎","青霉素"],
+  "1.2.156.112606.1.2.1.1.2.330.2010.4444":["赵六","上呼吸道感染","无"]
+}
+storage.set('data', InitialEMPIData, function (err) {
+  if (err) return console.error(err)
+})
